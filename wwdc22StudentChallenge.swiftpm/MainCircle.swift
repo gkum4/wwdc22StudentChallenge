06bf8ad021dd6c37a03f9d10940de5369c615e3a
gkum4 <gustavokumasawa@gmail.com>
2022-04-18 12:59:29 -0300

    MainCircle gradient

--- wwdc22StudentChallenge.swiftpm/MainCircle.swift
+++ wwdc22StudentChallenge.swiftpm/MainCircle.swift
@@ -8,6 +8,8 @@
 import SpriteKit
 
 class MainCircle: Circle {
+    private var gradientColors: [CGColor] = []
+    
     init(radius: CGFloat) {
         let circleColor = UIColor(named: "darkGray") ?? UIColor.darkGray
         
@@ -16,6 +18,72 @@ class MainCircle: Circle {
         node.name = Names.mainCircle
         circle.fillColor = circleColor
         circle.physicsBody?.isDynamic = false
+    }
+    
+    override func runChangeColorAnimation(
+        to newColor: UIColor,
+        withDuration duration: TimeInterval,
+        onCompletion: @escaping () -> Void = {}
+    ) {
+        if gradientColors.isEmpty {
+            super.runChangeColorAnimation(
+                to: newColor,
+                withDuration: duration,
+                onCompletion: onCompletion
+            )
+            
+            gradientColors.append(newColor.cgColor)
+            return
+        }
+        
+        gradientColors.append(newColor.cgColor)
+        
+        guard let newCircle = circle.copy() as? SKShapeNode else {
+            return
+        }
+        
+        newCircle.fillTexture = SKTexture(image: buildGradientImage())
+        newCircle.alpha = 0
+        node.addChild(newCircle)
+        
+        circle.run(.sequence([
+            .wait(forDuration: duration),
+            .run {
+                self.circle.removeFromParent()
+            }
+        ]))
+        
+        newCircle.run(.sequence([
+            .fadeIn(withDuration: duration+0.01),
+            .run {
+                self.circle = newCircle
+                onCompletion()
+            }
+        ]))
+    }
+    
+    private func buildGradientImage() -> UIImage {
+        let gradientLayer = CAGradientLayer()
+        gradientLayer.frame = circle.frame
+        gradientLayer.colors = gradientColors
+        gradientLayer.type = .radial
+        gradientLayer.startPoint = CGPoint(x: 1, y: 1)
+        gradientLayer.endPoint = CGPoint(x: 0, y: 0)
+        
+        UIGraphicsBeginImageContext(gradientLayer.bounds.size)
+        guard let currentContext = UIGraphicsGetCurrentContext() else {
+            UIGraphicsEndImageContext()
+            return UIImage()
+        }
+        gradientLayer.render(in: currentContext)
+        
+        guard let image = UIGraphicsGetImageFromCurrentImageContext() else {
+            UIGraphicsEndImageContext()
+            return UIImage()
+        }
+        
+        UIGraphicsGetCurrentContext()
+        return image
     }
     
     enum Names {
